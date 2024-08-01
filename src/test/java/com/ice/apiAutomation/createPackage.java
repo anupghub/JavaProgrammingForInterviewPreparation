76ea12819396d65659819a9f68c5b52914ffec54
unknown <anuprimt@gmail.com>
2024-08-01 16:21:53 +0530

    API automation

---
+++ src/test/java/com/ice/apiAutomation/createPackage.java
@@ -0,0 +1,12 @@
+package com.ice.apiAutomation;
+
+import org.testng.annotations.Test;
+
+public class createPackage {
+
+	
+	@Test
+	public void createPackage() {
+		System.out.println("Test2");
+	}
+}
---
+++ src/test/java/com/ice/apiAutomation/createPayloadTest.java
@@ -0,0 +1,10 @@
+package com.ice.apiAutomation;
+
+import org.testng.annotations.Test;
+
+public class createPayloadTest {
+  @Test
+  public void Test() {
+	  System.out.println("Test");
+  }
+}
